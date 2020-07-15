c1ed970cdcc6ab14a5052e6c4b94e4c7ba42000a
NPException <npexception42@gmail.com>
2020-07-15 19:11:22 +0200

    update dependencies

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "https://github.com/NPException/Clojure-Quotes-Bot"
   :license {:name "MIT License"
             :url "https://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [http-kit "2.2.0"]
-                 [org.clojure/data.json "0.2.6"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [http-kit "2.3.0"]
+                 [org.clojure/data.json "0.2.7"]]
   :jvm-opts ["--add-modules" "java.xml.bind"]
   :main ^:skip-aot cljqbot.core
   :target-path "target/%s"
